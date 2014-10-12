21ae9d9cd1cb3f28c361d7645aeafdcd569f2a44
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2014-10-12 05:58:00 -0300

    Minor change

--- app/code/local/PabloFiumara/SpecialCategory/controllers/IndexController.php
+++ app/code/local/PabloFiumara/SpecialCategory/controllers/IndexController.php
@@ -6,7 +6,7 @@ class PabloFiumara_SpecialCategory_IndexController extends Mage_Adminhtml_Contro
     {
         $this->loadLayout();
          
-        $block = $this->getLayout()->createBlock('core/text', 'green-block')->setText('<h3>hello</h1>');
+        $block = $this->getLayout()->createBlock('core/text', 'green-block')->setText('<h3>hello from indexAction()</h1>');
         $this->_addContent($block);
          
         $this->_setActiveMenu('specialcategory')->renderLayout();
