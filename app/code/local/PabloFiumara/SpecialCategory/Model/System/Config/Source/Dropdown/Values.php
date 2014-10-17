8106c94f86ef5515c8042c15172c56b7763886b5
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2014-10-17 16:23:21 -0300

    Added Model which has values to be displayed on Dropdown

---
+++ app/code/local/PabloFiumara/SpecialCategory/Model/System/Config/Source/Dropdown/Values.php
@@ -0,0 +1,18 @@
+<?php
+
+class PabloFiumara_SpecialCategory_Model_System_Config_Source_Dropdown_Values
+{
+    public function toOptionArray()
+    {
+        return array(
+            array(
+                'value' => 'key1',
+                'label' => 'Value Number 1',
+            ),
+            array(
+                'value' => 'key2',
+                'label' => 'Value Number 2',
+            ),
+        );
+    }
+}
