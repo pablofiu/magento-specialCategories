df0d2fec94d49a317cd88c334a6522667460a7ea
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2014-10-21 15:30:37 -0300

    Added a dropdown menu with categories

--- app/code/local/PabloFiumara/SpecialCategory/Model/System/Config/Source/Dropdown/Values.php
+++ app/code/local/PabloFiumara/SpecialCategory/Model/System/Config/Source/Dropdown/Values.php
@@ -5,25 +5,15 @@ class PabloFiumara_SpecialCategory_Model_System_Config_Source_Dropdown_Values
     public function toOptionArray()
     {
         $categories = Mage::getModel('catalog/category')->getCollection()->addAttributeToSelect('*')->addIsActiveFilter();
-        $keyNumber = 1;
-        $emptyArray = array();
+        $categoryNumber = 1;
+        $categoriesList = array();
         foreach ($categories as $obj) {
-            $aCategory = $obj->getName();
-            $array = array("value" => $keyNumber,"label" => $obj->getName(),);
-            ++$keyNumber;
+            $array = array("value" => $categoryNumber,"label" => $obj->getName(),);
+            array_push($categoriesList, $array);
+            ++$categoryNumber;
         }
 
-        $A = array(
-            array('value' => 'key1', 'label' => 'Value 1'),
-            array('value' => 'key2', 'label' => 'Value 2'));
-
-        $B = array(
-            array('value' => 'key3', 'label' => 'Value 3'),
-            array('value' => 'key4', 'label' => 'Value 4'));
-
-        $C = array_merge_recursive($A, $B);
-
-        return $C;
+        return $categoriesList;
 
     }
 
