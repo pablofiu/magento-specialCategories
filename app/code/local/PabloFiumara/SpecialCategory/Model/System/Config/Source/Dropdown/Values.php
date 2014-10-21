fa23b2217289a5e2e1da81881c9b822f5f3df72a
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2014-10-21 14:50:11 -0300

    Multiple values in dropdown

--- app/code/local/PabloFiumara/SpecialCategory/Model/System/Config/Source/Dropdown/Values.php
+++ app/code/local/PabloFiumara/SpecialCategory/Model/System/Config/Source/Dropdown/Values.php
@@ -5,27 +5,38 @@ class PabloFiumara_SpecialCategory_Model_System_Config_Source_Dropdown_Values
     public function toOptionArray()
     {
         $categories = Mage::getModel('catalog/category')->getCollection()->addAttributeToSelect('*')->addIsActiveFilter();
-        // $keyNumber = 'key1';
+        $keyNumber = 1;
+        $emptyArray = array();
         foreach ($categories as $obj) {
-        //$array1 = array('value' => 'key1', 'label' => $obj->getName(),);
-        // echo '<pre>'; print_r($array1); echo '</pre>';
+            $aCategory = $obj->getName();
+            // echo $aCategory;
+            $array = array("value" => $keyNumber,"label" => $obj->getName(),);
+            ++$keyNumber;
+            //var_dump($array);
+            //$emptyArray[$key] = $value;
+
+
+        }
+/*
         $test = array(
         array( 'value' => 'key1', 'label' => 'Value 1',)
         ,array('value' => 'key2','label' => 'Value 2',)
         ,);
-        }
-        return $test;
-        /*
-        return array(
-            array(
-                'value' => 'key1',
-                'label' => 'Value Number 1',
-            ),
-            array(
-                'value' => 'key2',
-                'label' => 'Value Number 2',
-            ),
-        );
-        */
+*/
+        $A = array(
+            array('value' => 'key1', 'label' => 'Value 1'),
+            array('value' => 'key2', 'label' => 'Value 2'));
+
+        $B = array(
+            array('value' => 'key3', 'label' => 'Value 3'),
+            array('value' => 'key4', 'label' => 'Value 4'));
+
+
+        $C = array_merge($A, $B);
+
+        return $C;
+
     }
 }
+
+
