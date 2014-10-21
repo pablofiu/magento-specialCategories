c43277c940a754bd55dc7e6a5a95321b746f7df6
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2014-10-21 11:15:21 -0300

    Working code: Dropdown needs to be populated with categories and subcategories

--- app/code/local/PabloFiumara/SpecialCategory/Model/System/Config/Source/Dropdown/Values.php
+++ app/code/local/PabloFiumara/SpecialCategory/Model/System/Config/Source/Dropdown/Values.php
@@ -4,6 +4,18 @@ class PabloFiumara_SpecialCategory_Model_System_Config_Source_Dropdown_Values
 {
     public function toOptionArray()
     {
+        $categories = Mage::getModel('catalog/category')->getCollection()->addAttributeToSelect('*')->addIsActiveFilter();
+        // $keyNumber = 'key1';
+        foreach ($categories as $obj) {
+        //$array1 = array('value' => 'key1', 'label' => $obj->getName(),);
+        // echo '<pre>'; print_r($array1); echo '</pre>';
+        $test = array(
+        array( 'value' => 'key1', 'label' => 'Value 1',)
+        ,array('value' => 'key2','label' => 'Value 2',)
+        ,);
+        }
+        return $test;
+        /*
         return array(
             array(
                 'value' => 'key1',
@@ -14,5 +26,6 @@ class PabloFiumara_SpecialCategory_Model_System_Config_Source_Dropdown_Values
                 'label' => 'Value Number 2',
             ),
         );
+        */
     }
 }
