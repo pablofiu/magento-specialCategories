68c131b24a4744e04153b0ed1c7fc6c3f60b7936
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2014-10-23 13:54:42 -0300

    Now values equal to all labels

--- app/code/local/PabloFiumara/SpecialCategory/Model/System/Config/Source/Dropdown/Values.php
+++ app/code/local/PabloFiumara/SpecialCategory/Model/System/Config/Source/Dropdown/Values.php
@@ -5,16 +5,12 @@ class PabloFiumara_SpecialCategory_Model_System_Config_Source_Dropdown_Values
     public function toOptionArray()
     {
         $categories = Mage::getModel('catalog/category')->getCollection()->addAttributeToSelect('*')->addIsActiveFilter();
-        $categoryNumber = 1;
         $categoriesList = array();
         foreach ($categories as $obj) {
-            $array = array("value" => $categoryNumber,"label" => $obj->getName(),);
+            $array = array("value" => $obj->getName(),"label" => $obj->getName(),);
             array_push($categoriesList, $array);
-            ++$categoryNumber;
         }
-
         return $categoriesList;
-
     }
 
 }
