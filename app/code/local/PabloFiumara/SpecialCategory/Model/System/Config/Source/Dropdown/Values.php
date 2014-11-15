c892b3d2619cc9e4ee648d94b1613ff7bd92e411
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2014-11-15 18:12:41 -0300

    Deleted from list of categories "New Arrivals" because that does not seem to be one of them

--- app/code/local/PabloFiumara/SpecialCategory/Model/System/Config/Source/Dropdown/Values.php
+++ app/code/local/PabloFiumara/SpecialCategory/Model/System/Config/Source/Dropdown/Values.php
@@ -7,12 +7,14 @@ class PabloFiumara_SpecialCategory_Model_System_Config_Source_Dropdown_Values
         $categories = Mage::getModel('catalog/category')->getCollection()->addAttributeToSelect('*')->addIsActiveFilter();
         $categoriesList = array();
         foreach ($categories as $obj) {
-            if ($obj->getName() == 'Default Category' || !$obj->hasChildren()) {
-            $array = array("value" => $obj->getName(),"label" => $obj->getName(),);
-            if ($obj->getName() == 'Default Category') {
-                $array = array("value" => $obj->getName(),"label" => "No Special Categories",);
-            }
-            array_push($categoriesList, $array);
+            if ($obj->getName() !== 'New Arrivals') {
+                if ($obj->getName() == 'Default Category' || !$obj->hasChildren()) {
+                    $array = array("value" => $obj->getName(),"label" => $obj->getName(),);
+                    if ($obj->getName() == 'Default Category') {
+                        $array = array("value" => $obj->getName(),"label" => "No Special Categories",);
+                    }
+                    array_push($categoriesList, $array);
+                }
             }
         }
         return $categoriesList;
