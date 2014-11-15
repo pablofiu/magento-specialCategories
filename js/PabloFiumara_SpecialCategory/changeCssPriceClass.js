4f9a2f68cb42cda496067b304debdab001157d94
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2014-11-15 14:52:31 -0300

    Added javascript file to frontend

---
+++ js/PabloFiumara_SpecialCategory/changeCssPriceClass.js
@@ -0,0 +1,3 @@
+console.log("from inside javascript frontend file");
+variable = $$('.regular-price .price')[1].hasClassName(regular-price);
+console.log("before going away from inside javascript frontend file");
